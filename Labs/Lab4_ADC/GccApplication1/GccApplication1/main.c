87ad2551a2025d9bac37e7cbe28ccffd9b49192c
zsha758 <zsha758@aucklanduni.ac.nz>
2026-09-14 23:01:16 +1200

    code for 4.4

--- Labs/Lab4_ADC/GccApplication1/GccApplication1/main.c
+++ Labs/Lab4_ADC/GccApplication1/GccApplication1/main.c
@@ -1,10 +1,3 @@
-/*
- * GccApplication1.c
- *
- * Created: 2026/9/14 21:05:26
- * Author : 941107
- */ 
-
 #include "common.h"
 #include <util/delay.h>
 #include "adc.h"
@@ -12,9 +5,8 @@
 
 #define NUM_SAMPLES 40
 
-// UART help function
 void uart_init(void) {
-	UBRR0L = 12; // 9600 Baud @ F_CPU = 2MHz
+	UBRR0L = 12;
 	UCSR0B = (1 << TXEN0);
 	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
@@ -25,35 +17,47 @@ void uart_send_char(char c) {
 }
 
 void uart_send_str(const char* s) {
-	while (*s) uart_send_char(*s++);
+	while (*s) {
+		uart_send_char(*s++);
+	}
 }
 
 void uart_send_num(uint16_t num) {
 	char buf[6];
 	int i = 0;
-	if (num == 0) { uart_send_char('0'); return; }
-	while (num > 0) { buf[i++] = (num % 10) + '0'; num /= 10; }
-	while (i > 0) uart_send_char(buf[--i]);
+	if (num == 0) {
+		uart_send_char('0');
+		return;
+	}
+	while (num > 0) {
+		buf[i++] = (num % 10) + '0';
+		num /= 10;
+	}
+	while (i > 0) {
+		uart_send_char(buf[--i]);
+	}
 }
-
+// ----------------------------
 
 int main(void) {
-	uint16_t rv1_samples[NUM_SAMPLES];
+	uint16_t adc0_samples[NUM_SAMPLES];
+	uint16_t adc1_samples[NUM_SAMPLES];
 
 	adc_init();
 	uart_init();
 
 	while (1) {
-		// get samples from ADC2
 		for (uint8_t i = 0; i < NUM_SAMPLES; i++) {
-			rv1_samples[i] = adc_convert_mv(adc_read(2));
+			adc0_samples[i] = adc_convert_mv(adc_read(0));
+			adc1_samples[i] = adc_convert_mv(adc_read(1));
 			_delay_ms(1);
 		}
 
-		//print
-		uart_send_str("RV1_mV\r\n");
+		uart_send_str("ADC0_mV, ADC1_mV\r\n");
 		for (uint8_t i = 0; i < NUM_SAMPLES; i++) {
-			uart_send_num(rv1_samples[i]);
+			uart_send_num(adc0_samples[i]);
+			uart_send_str(", ");
+			uart_send_num(adc1_samples[i]);
 			uart_send_str("\r\n");
 		}
 		uart_send_str("\r\n");
